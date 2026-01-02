70486723f7042258119b18775b4d20189694d3e4
ramanbhatia3 <bhattiramandeep3105@gmail.com>
2026-01-02 19:57:57 +0530

    fs/promises

---
+++ 04-learn-fs-promises.js
@@ -0,0 +1,9 @@
+// import fs from "fs/promises";
+
+
+// let result = await fs.readFile("./02-test.txt")
+// console.log(result.toString())
+
+// let input = await fs.writeFile("./02-test.txt",`\nThis code is from fs/promises`)
+
+// let input = await fs.appendFile("./02-test.txt",`\nThis code is from fs/promises`)
