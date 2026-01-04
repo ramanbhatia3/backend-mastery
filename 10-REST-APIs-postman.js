59abbfb98c9035a64e76a9a202723c9d374ff099
ramanbhatia3 <bhattiramandeep3105@gmail.com>
2026-01-05 01:27:21 +0530

    postman - get, post

---
+++ 10-REST-APIs-postman.js
@@ -0,0 +1,66 @@
+const express = require("express")
+const fs = require("fs")
+const users = require("./09-data.json")
+
+const app = express();
+const PORT = 8000
+
+// Middlware - Plugin
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/users",(req, res)=>{
+    const html = `
+        <ul>
+            ${users.map((user)=> `<li>${user.first_name}</li>`).join("")}
+        </ul>
+    `;
+    res.send(html)
+})
+
+app.get("/api/users",(req, res)=>{
+    return res.json(users)
+})
+
+app.get("/api/users/:id",(req, res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id)
+    return res.json(user)
+})
+
+app.post("/api/users",(req, res)=>{
+    const body = req.body
+    // console.log("BODY:",body)
+    users.push({...body,id: users.length + 1})
+    fs.writeFile("./09-data.json",JSON.stringify(users),(err, data) => {
+        return res.json({ status: "success", id: users.length })
+    })
+})
+
+app.patch("/api/users/:id",(req, res)=>{
+    // TODO: Edit the user with id
+    return res.json({ status: "pending" })
+})
+
+app.delete("/api/users/:id",(req, res)=>{
+    // TODO: Delete the user with id
+    return res.json({ status: "pending" })
+})
+
+// Merge
+
+// app.route('/api/users/:id').get((req, res)=>{
+//     const id = Number(req.params.id);
+//     const user = users.find((user) => user.id === id)
+//     return res.json(user)
+// })
+// .patch((req,res) => {
+//     // Edit user with id
+//     return res.json({ status: "pending" })
+// })
+// .delete((req,res) => {
+//     // Delete user with id
+//     return res.json({ status: "pending" })
+// })
+
+
+app.listen(PORT, () => console.log(`Server started  at PORT:${PORT}`))
