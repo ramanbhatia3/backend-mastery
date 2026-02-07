0bb8d4aab18fb213afbe3d0eb29c54436dc9f8c6
ramanbhatia3 <bhattiramandeep3105@gmail.com>
2026-02-08 00:38:31 +0530

    multer

---
+++ 20-multer/index.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const express = require('express');
+const multer = require('multer');
+
+const app = express();
+const PORT = 8000;
+
+const storage = multer.diskStorage({
+    destination: function (req, res, cb){
+        return cb(null, "./uploads")
+    },
+    filename: function (req, file, cb) {
+        return cb(null, `${Date.now()}-${file.originalname}`)
+    }
+})
+
+const upload = multer({storage});
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'))
+
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.render('homepage');
+});
+
+app.post("/upload", upload.single("profileImage"), (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  
+  return res.redirect("/");
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
