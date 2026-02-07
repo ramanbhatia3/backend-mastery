71178fd41ce53fd60ea7b5f30bad557c35d3f959
ramanbhatia3 <bhattiramandeep3105@gmail.com>
2026-02-07 23:59:37 +0530

    authorization

---
+++ 19-authorization/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+
+const cookieParser = require("cookie-parser");
+
+const { connectToMongoDB } = require("./connect.js")
+
+const { checkForAuthentication, restrictTo } = require("./middleware/auth.js")
+const URL = require("./models/url.js")
+
+const urlRoute = require("./routes/url.js")
+const staticRoute = require("./routes/staticRouter.js")
+const userRoute = require("./routes/user.js")
+
+
+const app = express();
+const PORT = 8000;
+
+connectToMongoDB("mongo url here").then(() => console.log("MongoDB Connected Successfully!")).catch((err) => console.log("MongoDb Connection Failed:", err));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser());
+app.use(checkForAuthentication);
+
+app.use("/url", restrictTo(["NORMAL", "ADMIN"]), urlRoute);
+app.use("/user", userRoute)
+app.use("/", staticRoute);
+
+
+app.get("/url/:shortID", async (req, res) => {
+  const shortID = req.params.shortID;
+  const entry = await URL.findOneAndUpdate({
+    shortID
+  }, {
+    $push: {
+      visitHistory: { timestamp: Date.now() }
+    }
+  })
+  res.redirect(entry.redirectURL);
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
