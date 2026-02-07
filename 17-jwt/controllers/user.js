51f328d86133626f6700f8bea01b3fefce957638
ramanbhatia3 <bhattiramandeep3105@gmail.com>
2026-02-07 18:49:44 +0530

    jsonwebtoken

---
+++ 17-jwt/controllers/user.js
@@ -0,0 +1,33 @@
+const { v4: uuidv4 } = require("uuid")
+
+const User = require("../models/user.js")
+
+const { setUser } = require("../service/auth.js")
+
+async function handleUserSignUp(req, res){
+    const { name, email, password } = req.body;
+
+    await User.create({
+        name, email, password
+    })
+    return res.redirect("/")
+}
+
+async function handleUserLogIn(req, res){
+    const { name, email, password } = req.body;
+
+    const user = await User.findOne({ email, password })
+
+    if (!user) return res.render("login", {
+        error: "Invalid Credentials"
+    })
+
+    const token = setUser(user)
+    res.cookie("uid", token);
+    return res.redirect("/")
+}
+
+module.exports = {
+    handleUserSignUp,
+    handleUserLogIn
+}
